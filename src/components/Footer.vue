d19c1550e5b38009f1f6e70ecf4f73da4e5067c6
datu1210 <turizod@gmail.com>
2020-12-16 10:47:13 -0500

    footer terminado

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="page-footer font-small teal pt-4">
+  <footer class="page-footer font-small teal pt-4 bg-light margin-top:10%">
     <!-- Footer Text -->
     <div class="container-fluid text-center text-md-left">
       <!-- Grid row -->
@@ -8,24 +8,25 @@
         <div class="col">
           <!-- Content -->
 
-          <div class="footer-copyright text-center py-3">
+          <div class="footer-copyright text-center py-4 bg-light">
             <div id="recuadros_articulo_13383" class="col">
               <div class="recuadro">
                 <h4
                   class="titulo title-pill title-pill-secondary arrow text-uppercase aid-150236 cid-645"
                 >
-                  <a href="/portal/Secciones/Mision-TIC-2022/"
+                  <a
+                    href="https://www.misiontic2022.gov.co/portal/Secciones/Mision-TIC-2022/"
                     >Conoce la Misión TIC</a
                   >
                 </h4>
-                <h6 class="abstract mt-4 pb-5 aid-150236 cid-645">
+                <p class="abstract mt-4 pb-5 aid-150236 cid-645 text-justify">
                   La Misión TIC 2022 es un programa liderado por el Gobierno del
                   Presidente Iván Duque con un gran objetivo:  formar 100.000
                   jóvenes y adultos colombianos en programación, para enfrentar
                   los desafíos de la Cuarta Revolución Industrial. En esta
                   misión, mujeres y hombres serán elegidos y formados para ser
                   parte de la aventura que propulsará el mundo hacia adelante.
-                </h6>
+                </p>
               </div>
             </div>
           </div>
@@ -37,21 +38,45 @@
         <!-- Grid column -->
         <div class="col-md-6 mb-md-0 mb-3">
           <!-- Content -->
-          <h2 class="text-uppercase font-weight-bold">CONTACTANOS</h2>
-          <div>
-            <h6 class="text-uppercase font-weight-bold">
-              Jurgen: 324 458 6034
-            </h6>
-            <h6 class="text-uppercase font-weight-bold">
-              Wiliam: 324 459 6537
-            </h6>
-            <h6 class="text-uppercase font-weight-bold">David: 324 457 6736</h6>
-            <h6 class="text-uppercase font-weight-bold">
-              Daniela: 324 455 6331
-            </h6>
-            <h6 class="text-uppercase font-weight-bold">
-              Marisol: 324 454 6439
-            </h6>
+          <div class="footer-copyright text-center py-4 bg-light">
+            <div id="recuadros_articulo_13383" class="col">
+              <div class="recuadro">
+                <h4
+                  class="titulo title-pill title-pill-secondary2 arrow text-uppercase aid-150236 cid-645"
+                >
+                  <a>NUESTROS CONTACTOS</a>
+                </h4>
+                <div class="row">
+                  <div class="col-sm-6" style="margin-top:1%;text-align:right;">
+                    <h5>Jurgen Orjuela</h5>
+                    <h6>Tel. 530 234 7678</h6>
+                  </div>
+                  <div class="col-sm-6" style="margin-top:1%;text-align:left;">
+                    <h5>William Vargas</h5>
+                    <h6>Tel. 530 234 7678</h6>
+                  </div>
+                </div>
+
+                 <div class="row">
+                  <div class="col-sm-6" style="margin-top:1%;text-align:right;">
+                    <h5>David Turizo</h5>
+                    <h6>Tel. 530 234 7678</h6>
+                  </div>
+                  <div class="col-sm-6" style="margin-top:1%;text-align:left;">
+                    <h5>Daniela LaVerde</h5>
+                    <h6>Tel. 530 234 7678</h6>
+                  </div>
+                </div>
+
+                                 <div class="row">
+                  <div class="col-sm-12" style="margin-top:1%">
+                    <h5>Marisol Osma</h5>
+                    <h6>Tel. 530 234 7678</h6>
+                  </div>
+                </div>
+
+              </div>
+            </div>
           </div>
         </div>
         <!-- Grid column -->
@@ -61,7 +86,7 @@
     <!-- Footer Text -->
 
     <!-- Copyright -->
-    <div class="footer-copyright text-center py-3">
+    <div class="footer-copyright2 text-center py-3 bg-dark">
       Enlace a nuestro Repositorio:
       <a href="https://github.com/Orjuela91/proyecto_final_ciclo3-51"
         >ir a GitHub</a
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 
+
 Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
