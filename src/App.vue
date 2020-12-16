65c4e997442f0b28bf25aa9dcc2ae8e2f5a4caf4
SolMari1 <osmallanesmgmail.com>
2020-12-16 11:02:56 -0500

    miembros

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,14 @@
     <Noticias/>
 
     <Equipo/>
+
+    <div class="container-fluid">
+      <div class="row justify-content-center mb-5">
+        <div class="col mt-5" v-for="(miembro, index) of Team" :key="index">
+          <team-card :member="miembro" > </team-card>
+        </div>
+      </div>
+    </div>
     
     <Footer/>
     
@@ -52,7 +60,45 @@ export default {
   },
   methods: {},
   data() {
-    return {};
+    return {
+       Team:[
+        {
+        codigo : 1 ,
+        nombre : 'Jurgen Orjuela' ,
+        descripcion : 'leer, videojuegos, estudiar.' ,
+        rol : 'Desarrollador de sofware' ,
+        image : 'https://i.imgur.com/XuOvYfE.jpg'
+        },
+        {
+        codigo : 2 ,
+        nombre : 'David Turizo' ,
+        descripcion : 'Leer, Musica,Tocar la guitarra.' ,
+        rol : 'Ingeniero Electronico' ,
+        image : 'https://i.imgur.com/j2eUbQM.jpg'
+        },
+        {
+        codigo : 3 ,
+        nombre : 'William Vargas' ,
+        descripcion : 'Ver series.' ,
+        rol : 'Estudiante de Ingenieria Mecatronica' ,
+        image : 'https://i.imgur.com/jE7KCYY.jpg'
+        },
+        {
+        codigo : 4 ,
+        nombre : 'Daniela Laverde' ,
+        descripcion : 'Leer,Crear,Ver series.' ,
+        rol : 'Psicóloga' ,
+        image : 'https://i.imgur.com/wPcCF2m.jpg'
+        },
+        {
+        codigo : 5 ,
+        nombre : 'Marisol Osma' ,
+        descripcion : 'Dibujar,Tocar el violin.' ,
+        rol : 'Estudiante de musica' ,
+        image : 'https://i.imgur.com/OInkAD8.jpg'
+        }
+      ]
+    };
   },
 };
 </script>
